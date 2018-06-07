35492cd271851339e392a265b691264f491e4b7a
Porrith Suong <psuong95@gmail.com>
2018-06-07 07:29:58 -0400

    Adds a triple.

--- Tuples/Tuple.cs
+++ Tuples/Tuple.cs
@@ -24,4 +24,20 @@ namespace CommonStructures {
             return new Tuple<T1, T2>(item1, item2);
         }
     }
+
+    public struct Tuple<T1, T2, T3> {
+        public readonly T1 item1;
+        public readonly T2 item2;
+        public readonly T3 item3;
+
+        public Tuple(T1 item1, T2 item2, T3 item3) {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+        public static Tuple<T1, T2, T3> CreateTuple(T1 item1, T2 item2, T3 item3) {
+            return new Tuple<T1, T2, T3>(item1, item2, item3);
+        }
+    }
 }
