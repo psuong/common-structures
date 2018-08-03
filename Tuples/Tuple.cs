ce847fbfc78204c20972883d9b46b9de9628f353
Porrith Suong <psuong95@gmail.com>
2018-08-03 19:14:11 -0400

    Adds System.Serializable Attributes and docStrings to the triple.

--- Tuples/Tuple.cs
+++ Tuples/Tuple.cs
@@ -4,8 +4,9 @@ namespace CommonStructures {
     /// Tuples exists already in C# but as reference based values. This struct provides a value based 
     /// tuple with immutable fields.
     /// </summary>
+    [System.Serializable]
     public struct Tuple<T1, T2> {
-
+        
         public readonly T1 item1;
         public readonly T2 item2;
 
@@ -22,6 +23,34 @@ namespace CommonStructures {
         /// <returns>An ordered pair</returns>
         public static Tuple<T1, T2> CreateTuple(T1 item1, T2 item2) {
             return new Tuple<T1, T2>(item1, item2);
+        }
+    }
+
+    /// <summary>
+    /// A triple with immutable fields that is value based.
+    /// </summary>
+    [System.Serializable]
+    public struct Tuple<T1, T2, T3> {
+
+        public readonly T1 item1;
+        public readonly T2 item2;
+        public readonly T3 item3;
+
+        public Tuple(T1 item1, T2 item2, T3 item3) {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+        
+        /// <summary>
+        /// Utility method to create a tuple of 3 arguments that are generic types.
+        /// </summary>
+        /// <param name="item1">The first element in the triple.</param>
+        /// <param name="item2">The second element in the triple.</param>
+        /// <param name="item3">The third element in the triple.</param>
+        /// <returns>An ordered triple.</returns>
+        public static Tuple<T1, T2, T3> CreateTuple(T1 item1, T2 item2, T3 item3) {
+            return new Tuple<T1, T2, T3>(item1, item2, item3);
         }
     }
 }
