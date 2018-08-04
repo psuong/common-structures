3ff99eabc7e715b7a0a82efed61e2bd1b2c2e926
Porrith Suong <psuong95@gmail.com>
2018-08-04 10:50:49 -0400

    Adds a four tuple.

--- Tuples/Tuple.cs
+++ Tuples/Tuple.cs
@@ -53,4 +53,35 @@ namespace CommonStructures {
             return new Tuple<T1, T2, T3>(item1, item2, item3);
         }
     }
+
+    /// <summary>
+    /// A four tuple with immutable fields that is value based.
+    /// </summary>
+    [System.Serializable]
+    public struct Tuple<T1, T2, T3, T4> {
+
+        public readonly T1 item1;
+        public readonly T2 item2;
+        public readonly T3 item3;
+        public readonly T4 item4;
+
+        public Tuple(T1 item1, T2 item2, T3 item3, T4 item4) {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+            this.item4 = item4;
+        }
+        
+        /// <summary>
+        /// Utility method to create a tuple of 3 arguments that are generic types.
+        /// </summary>
+        /// <param name="item1">The first element in the four-tuple.</param>
+        /// <param name="item2">The second element in the four-tuple.</param>
+        /// <param name="item3">The third element in the four-tuple.</param>
+        /// <param name="item4">The fourth element in the four-tuple.</param>
+        /// <returns>An ordered four-tuple.</returns>
+        public static Tuple<T1, T2, T3, T4> CreateTuple(T1 item1, T2 item2, T3 item3, T4 item4) {
+            return new Tuple<T1, T2, T3, T4>(item1, item2, item3, item4);
+        }
+    }
 }
