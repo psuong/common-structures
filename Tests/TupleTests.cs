2d3a76b048365dc41eb33ccfaaccdd1b77d8fac8
Porrith Suong <psuong95@gmail.com>
2018-10-07 20:43:01 -0400

    Adds equality support for Tuples.

--- Tests/TupleTests.cs
+++ Tests/TupleTests.cs
@@ -10,21 +10,21 @@ namespace CommonStructures.Tests {
 
         [Test]
         public void TestDouble() {
-            var @double = Tuple.CreateTuple(0, 1);
+            var @double = Tuple.Create(0, 1);
             Assert.AreEqual(0, @double.item1, "Ordered double mismatch");
             Assert.AreEqual(1, @double.item2, "Ordered double mismatch");
         }
 
         [Test]
         public void TestTriples() {
-            var triple = Tuple.CreateTuple(0, "A String", 'c');
+            var triple = Tuple.Create(0, "A String", 'c');
             Assert.AreEqual(0, triple.item1, "Value mismatch!");
             Assert.AreEqual("A String", triple.item2, "Value mismatch!");
         }
 
         [Test]
         public void TestFourTuples() {
-            var fourTuple = Tuple.CreateTuple(0f, 1f, 2f, 3f);
+            var fourTuple = Tuple.Create(0f, 1f, 2f, 3f);
             Assert.AreEqual(0f, fourTuple.item1, "Value mismatch!");
             Assert.AreEqual(1f, fourTuple.item2, "Value mismatch!");
             Assert.AreEqual(2f, fourTuple.item3, "Value mismatch!");
@@ -33,7 +33,7 @@ namespace CommonStructures.Tests {
 
         [Test]
         public void TestFiveTuples() {
-            var fiveTuple = Tuple.CreateTuple('a', 'b', 'c', 'd', 'e');
+            var fiveTuple = Tuple.Create('a', 'b', 'c', 'd', 'e');
             Assert.AreEqual('a', fiveTuple.item1, "Value mismatch!");
             Assert.AreEqual('b', fiveTuple.item2, "Value mismatch!");
             Assert.AreEqual('c', fiveTuple.item3, "Value mismatch!");
@@ -43,7 +43,7 @@ namespace CommonStructures.Tests {
 
         [Test]
         public void TestSexTuple() {
-            var sexTuple = Tuple.CreateTuple('a', "lot of different values", 1, 2f, 3.0, true);
+            var sexTuple = Tuple.Create('a', "lot of different values", 1, 2f, 3.0, true);
             Assert.AreEqual('a', sexTuple.item1, "Value mismatch!");
             Assert.AreEqual("lot of different values", sexTuple.item2, "Value mismatch!");
             Assert.AreEqual(1, sexTuple.item3, "Value mismatch!");
@@ -54,7 +54,7 @@ namespace CommonStructures.Tests {
 
         [Test]
         public void TestSepTuple() {
-            var sepTuple = Tuple.CreateTuple(1, 2, 3, 4, 5, 6, 7);
+            var sepTuple = Tuple.Create(1, 2, 3, 4, 5, 6, 7);
             Assert.AreEqual(1, sepTuple.item1, "Value mismatch!");
             Assert.AreEqual(2, sepTuple.item2, "Value mismatch!");
             Assert.AreEqual(3, sepTuple.item3, "Value mismatch!");
@@ -66,7 +66,7 @@ namespace CommonStructures.Tests {
 
         [Test]
         public void TestOctTuple() {
-            var octTuple = Tuple.CreateTuple(1f, 2.0f, 3f, 4f, 5f, 6f, 7f, 8f);
+            var octTuple = Tuple.Create(1f, 2.0f, 3f, 4f, 5f, 6f, 7f, 8f);
             Assert.AreEqual(1f, octTuple.item1, "Value mismatch!");
             Assert.AreEqual(2.0f, octTuple.item2, "Value mismatch!");
             Assert.AreEqual(3f, octTuple.item3, "Value mismatch!");
--- Tuples/Tuple.cs
+++ Tuples/Tuple.cs
@@ -6,49 +6,84 @@ namespace CommonStructures {
     public struct Tuple {
 
         /// <summary> /// A utility function to create a pair.  /// </summary>
-        public static Tuple<T1, T2> CreateTuple<T1, T2>(T1 item1, T2 item2) {
+        public static Tuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2> {
             return new Tuple<T1, T2>(item1, item2);
         }
 
         /// <summary>
         /// A utility function to create a a triple.
         /// </summary>
-        public static Tuple<T1, T2, T3> CreateTuple<T1, T2, T3>(T1 item1, T2 item2, T3 item3) {
+        public static Tuple<T1, T2, T3> Create<T1, T2, T3>(T1 item1, T2 item2, T3 item3) 
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3> {
             return new Tuple<T1, T2, T3>(item1, item2, item3);   
         }
 
         /// <summary>
         /// A utility function to create a four tuple.
         /// </summary>
-        public static Tuple<T1, T2, T3, T4> CreateTuple<T1, T2, T3, T4>(T1 item1, T2 item2, T3 item3, T4 item4) {
+        public static Tuple<T1, T2, T3, T4> Create<T1, T2, T3, T4>(T1 item1, T2 item2, T3 item3, T4 item4) 
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3>
+            where T4 : System.IEquatable<T4> {
             return new Tuple<T1, T2, T3, T4>(item1, item2, item3, item4);
         }
 
         /// <summary>
         /// A utility function to create a five tuple.
         /// </summary>
-        public static Tuple<T1, T2, T3, T4, T5> CreateTuple<T1, T2, T3, T4, T5>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5) {
+        public static Tuple<T1, T2, T3, T4, T5> Create<T1, T2, T3, T4, T5>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)  
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3>
+            where T4 : System.IEquatable<T4> 
+            where T5 : System.IEquatable<T5> {
             return new Tuple<T1, T2, T3, T4, T5>(item1, item2, item3, item4, item5);
         }
 
         /// <summary>
         /// A utility function to create a six tuple.
         /// </summary>
-        public static Tuple<T1, T2, T3, T4, T5, T6> CreateTuple<T1, T2, T3, T4, T5, T6>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6) {
+        public static Tuple<T1, T2, T3, T4, T5, T6> Create<T1, T2, T3, T4, T5, T6>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6) 
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3>
+            where T4 : System.IEquatable<T4> 
+            where T5 : System.IEquatable<T5>
+            where T6 : System.IEquatable<T6> {
             return new Tuple<T1, T2, T3, T4, T5, T6>(item1, item2, item3, item4, item5, item6);
         }
 
         /// <summary>
         /// A utility function to create a seven tuple.
         /// </summary>
-        public static Tuple<T1, T2, T3, T4, T5, T6, T7> CreateTuple<T1, T2, T3, T4, T5, T6, T7>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7) {
+        public static Tuple<T1, T2, T3, T4, T5, T6, T7> Create<T1, T2, T3, T4, T5, T6, T7>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7) 
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3>
+            where T4 : System.IEquatable<T4> 
+            where T5 : System.IEquatable<T5>
+            where T6 : System.IEquatable<T6>
+            where T7 : System.IEquatable<T7> {
             return new Tuple<T1, T2, T3, T4, T5, T6, T7>(item1, item2, item3, item4, item5, item6, item7);
         }
 
         /// <summary>
         /// A utility function to create an octuple.
         /// </summary>
-        public static Tuple<T1, T2, T3, T4, T5, T6, T7, T8> CreateTuple<T1, T2, T3, T4, T5, T6, T7, T8>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7, T8 item8) {
+        public static Tuple<T1, T2, T3, T4, T5, T6, T7, T8> Create<T1, T2, T3, T4, T5, T6, T7, T8>(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6, T7 item7, T8 item8) 
+            where T1 : System.IEquatable<T1>
+            where T2 : System.IEquatable<T2>
+            where T3 : System.IEquatable<T3>
+            where T4 : System.IEquatable<T4> 
+            where T5 : System.IEquatable<T5>
+            where T6 : System.IEquatable<T6>
+            where T7 : System.IEquatable<T7>
+            where T8 : System.IEquatable<T8> {
             return new Tuple<T1, T2, T3, T4, T5, T6, T7, T8>(item1, item2, item3, item4, item5, item6, item7, item8);
         }
     }
@@ -58,7 +93,9 @@ namespace CommonStructures {
     /// tuple with immutable fields.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2> {
+    public struct Tuple<T1, T2> : System.IEquatable<Tuple<T1, T2>>
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2> {
         
         public readonly T1 item1;
         public readonly T2 item2;
@@ -67,13 +104,20 @@ namespace CommonStructures {
             this.item1 = item1;
             this.item2 = item2;
         }
+
+        public bool Equals(Tuple<T1, T2> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2);
+        }
     }
 
     /// <summary>
     /// A triple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3> {
+    public struct Tuple<T1, T2, T3> : System.IEquatable<Tuple<T1, T2, T3>>
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2>
+        where T3 : System.IEquatable<T3> {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -84,13 +128,21 @@ namespace CommonStructures {
             this.item2 = item2;
             this.item3 = item3;
         }
+
+        public bool Equals(Tuple<T1, T2, T3> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3);
+        }
     }
 
     /// <summary>
     /// A four tuple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3, T4> {
+    public struct Tuple<T1, T2, T3, T4> : System.IEquatable<Tuple<T1, T2, T3, T4>>
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2> 
+        where T3 : System.IEquatable<T3> 
+        where T4 : System.IEquatable<T4> {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -103,13 +155,22 @@ namespace CommonStructures {
             this.item3 = item3;
             this.item4 = item4;
         }
+
+        public bool Equals(Tuple<T1, T2, T3, T4> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3) && item4.Equals(other.item4);
+        }
     }
 
     /// <summary>
     /// A five tuple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3, T4, T5> {
+    public struct Tuple<T1, T2, T3, T4, T5> : System.IEquatable<Tuple<T1, T2, T3, T4, T5>> 
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2>
+        where T3 : System.IEquatable<T3>
+        where T4 : System.IEquatable<T4>
+        where T5 : System.IEquatable<T5> {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -124,13 +185,24 @@ namespace CommonStructures {
             this.item4 = item4;
             this.item5 = item5;
         }
+
+        public bool Equals(Tuple<T1, T2, T3, T4, T5> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3) && item4.Equals(other.item4) &&
+                item5.Equals(other.item5);
+        }
     }
 
     /// <summary>
     /// A six tuple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3, T4, T5, T6> {
+    public struct Tuple<T1, T2, T3, T4, T5, T6> : System.IEquatable<Tuple<T1, T2, T3, T4, T5, T6>> 
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2>
+        where T3 : System.IEquatable<T3>
+        where T4 : System.IEquatable<T4>
+        where T5 : System.IEquatable<T5>
+        where T6 : System.IEquatable<T6> {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -147,13 +219,25 @@ namespace CommonStructures {
             this.item5 = item5;
             this.item6 = item6;
         }
+
+        public bool Equals(Tuple<T1, T2, T3, T4, T5, T6> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3) && item4.Equals(other.item4) &&
+                item5.Equals(other.item5) && item6.Equals(other.item6);
+        }
     }
 
     /// <summary>
     /// A seven tuple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3, T4, T5, T6, T7> {
+    public struct Tuple<T1, T2, T3, T4, T5, T6, T7> : System.IEquatable<Tuple<T1, T2, T3, T4, T5, T6, T7>> 
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2>
+        where T3 : System.IEquatable<T3>
+        where T4 : System.IEquatable<T4>
+        where T5 : System.IEquatable<T5>
+        where T6 : System.IEquatable<T6>
+        where T7 : System.IEquatable<T7> {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -172,13 +256,25 @@ namespace CommonStructures {
             this.item6 = item6;
             this.item7 = item7;
         }
+
+        public bool Equals(Tuple<T1, T2, T3, T4, T5, T6, T7> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3) && item4.Equals(other.item4) &&
+                item5.Equals(other.item5) && item6.Equals(other.item6) && item7.Equals(other.item7);
+        }
     }
 
     /// <summary>
     /// An octuple with immutable fields that is value based.
     /// </summary>
     [System.Serializable]
-    public struct Tuple<T1, T2, T3, T4, T5, T6, T7, T8> {
+    public struct Tuple<T1, T2, T3, T4, T5, T6, T7, T8> : System.IEquatable<Tuple<T1, T2, T3, T4, T5, T6, T7, T8>> 
+        where T1 : System.IEquatable<T1>
+        where T2 : System.IEquatable<T2>
+        where T3 : System.IEquatable<T3>
+        where T4 : System.IEquatable<T4>
+        where T5 : System.IEquatable<T5>
+        where T6 : System.IEquatable<T6>
+        where T7 : System.IEquatable<T7>  {
 
         public readonly T1 item1;
         public readonly T2 item2;
@@ -198,6 +294,11 @@ namespace CommonStructures {
             this.item6 = item6;
             this.item7 = item7;
             this.item8 = item8;
+        }
+
+        public bool Equals(Tuple<T1, T2, T3, T4, T5, T6, T7, T8> other) {
+            return item1.Equals(other.item1) && item2.Equals(other.item2) && item3.Equals(other.item3) && item4.Equals(other.item4) &&
+                item5.Equals(other.item5) && item6.Equals(other.item6) && item7.Equals(other.item7) && item8.Equals(other.item8);
         }
     }
 }
